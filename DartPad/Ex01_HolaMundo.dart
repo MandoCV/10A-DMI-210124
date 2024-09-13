fd3bfad5ff98bb0d6131f4f2f5f35359bd6a4774
Mando CV <210124@utxicotepec.edu.mx>
2024-09-13 10:54:23 -0600

    Revisada la creación del archivo 'hola mundo': se confirmaron su existencia y contenido

---
+++ DartPad/Ex01_HolaMundo.dart
@@ -0,0 +1,18 @@
+void main(){
+  // Variable para almacenar mi nombre
+  var myName = 'Armando';
+  String myLastName = 'Carrasco';
+  final int myDNI = 210124;
+  //late final int myAge;
+  
+  //Corregir mi nombre
+  myName = "Armando Carrasco Vargas";
+  //myDNI = 210124; Esto ocasiono un error porque la variable  esta definida como final
+  
+  print('Hola, $myName $myLastName, tu matricula es $myDNI y tu edad aún no la conozco, por que no se cuando naciste');
+  
+  //Interpolando el valor  de las variables con metodos de manipulacion o transformacion
+    print('\nHola, ${myName.toUpperCase()} ${myLastName.toUpperCase()}, tu matricula es: $myDNI y tu edad aún no la conozco, por que no se cuando naciste');
+}
+
+// git commit -m "Practica 03" -m "Agregación de la carpeta de DARTPAD con el ejemplo de HolaMundo"
