0f3220324fd2f57a2f10ba33b5152b5a4410f76c
Mando CV <210124@utxicotepec.edu.mx>
2024-09-17 09:10:22 -0600

    Practicas 02
    Añadido el archivo ejercicio 02 y actualización de README

---
+++ DartPad/Ex02_Tipos_De_Datos.dart
@@ -0,0 +1,11 @@
+void main() {
+  final String pokemon = 'kunno';
+  final int hp = 100;
+  final bool isAlive = true;
+
+  print(""" Ek pokemon que  elegiste es:
+  
+  $pokemon, las caracteristicas de $pokemon, son: vida (hp) $hp
+  Estiatus de vida: $isAlive
+""");
+}
