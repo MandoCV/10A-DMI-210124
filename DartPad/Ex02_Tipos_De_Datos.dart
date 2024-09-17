5f6d73bb1f0a02a0db80820abf306aec96253502
Mando CV <210124@utxicotepec.edu.mx>
2024-09-17 09:11:34 -0600

    Practicas 02
    actualización de README

--- DartPad/Ex02_Tipos_De_Datos.dart
+++ DartPad/Ex02_Tipos_De_Datos.dart
@@ -3,9 +3,14 @@ void main() {
   final int hp = 100;
   final bool isAlive = true;
 
+    final habilities = ['Impact: trueno', 'Cola de hierro','Ataque Rapido'];
+  final sprites = <String> ['charizar_front.png', 'charizar_back.png'];
+
   print(""" Ek pokemon que  elegiste es:
   
   $pokemon, las caracteristicas de $pokemon, son: vida (hp) $hp
-  Estiatus de vida: $isAlive
+  Estiatus de vida: $isAlive.
+  sus habilidades son: $habilities
+  y su apariencia es: $sprites
 """);
 }
